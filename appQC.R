820ff1b73663280e721841bdb53cb87d77e2ba58
Nick Kolarik <nicholaskolarik@u.boisestate.edu>
2025-07-07 08:28:44 -0600

    init upload

---
+++ appQC.R
@@ -0,0 +1,116 @@
+## https://shiny.posit.co/r/getstarted/shiny-basics/
+
+## https://shiny.posit.co/r/gallery/widgets/widget-gallery/ more on widgets
+#install.packages('shiny')
+library(shiny)
+library(bslib)
+library(bsicons)
+library(vroom)
+library(utils)
+library(ggplot2)
+library(dplyr)
+
+source("./helpersQC.R")
+
+yankee = read.csv("./methods/yankee.csv")
+yankee = yankee %>%
+  mutate(date =as.Date(yankee[,1], format = "%b %e, %Y"))
+yankee
+
+#runExample("01_hello")
+# Define UI ----
+ui <- page_sidebar(
+  title = "Time series analysis for quantifying restoration impacts",
+  sidebar = sidebar(
+    helpText(
+        "Select an analysis method",
+        "and analyze your data. We have",
+        "collated methods from the manuscript titled: ",
+        "Time series analyses provide a low-cost and scalable way",
+        "to assess restoration outcomes from satellite data."
+      ),
+    fileInput("file1", label = "Time Series", accept = ".csv" ),
+    selectInput(
+        "select",
+        "Choose analysis method",
+        choices = list("BCP" , "BFAST", "BEAST" , "BSTS" ),
+        selected = "BCP" 
+      ),
+    dateInput("date", "Select date (for BSTS)", value = "2014-01-01"),
+    fileInput("file2", label = "Predictor (for BSTS)", accept = ".csv" ),
+    card(
+      card_header(""),
+      actionButton("action", "Submit")#https://shiny.posit.co/r/articles/build/action-buttons/
+    ),
+    card_image("./fig1.png")),## too small here but doesn't make sense in the main?
+    tableOutput("file"),
+    mainPanel(
+      #tableOutput("head"),
+      plotOutput("plot")
+  )
+)
+
+
+server <- function(input, output, session) {
+  
+  #output$file <- renderTable(input$file1)
+  
+  data <- reactive({
+    req(input$file1)
+    
+    ext <- tools::file_ext(input$file1$name)
+    switch(ext,
+           csv = vroom::vroom(input$file1$datapath, delim = ","),
+           tsv = vroom::vroom(input$file1$datapath, delim = "\t"),
+           validate("Invalid file; Please upload a .csv or .tsv file")
+    )
+  })
+  
+  #output$head <- renderTable({
+  #  req(data())
+  #  if (is.null(data())) {
+  #    return(NULL)
+  #  }   
+  #  head(data())
+  #  #head(yankee)
+  #})
+  
+  observeEvent(input$file1, {
+    message(paste0("File Chosen: ", input$file1))
+  })
+  
+  ## data
+  datafile = reactive({
+    input$file1$datapath
+  })
+  
+  ## predictor
+  predfile = reactive({
+    input$file2$datapath
+  })
+  
+  ## method
+  method = reactive({
+    input$select
+  })
+  
+  ## plot
+  output$plot = renderPlot({
+   req(data())
+   if (is.null(data())) {
+     return(NULL)
+   }
+  
+  make_plot(method(), datafile())
+  #make_plot(method(), datafile(), predfile())
+
+  })
+}
+#  observeEvent(input$action, {
+#    output$data = renderTable({
+#      head(df)
+#      
+#    })
+#  })
+### define the shiny object w ui and server obj params
+shinyApp(ui = ui, server = server)
---
+++ helpersQC.R
@@ -0,0 +1,113 @@
+library(bfast) ##BFAST
+library(terra)
+library(gtools)
+library(sf)
+library(bcp) ## Bayesian Change Point v4.0.3
+library(Rbeast) ## Bayesian Estimator of Abrupt and Seasonal Trends
+library(CausalImpact) ## Bayesian Structural Time Series
+library(zoo)
+
+
+#make_plot = function(method, file, ancFiles = NULL, date = NULL){
+make_plot = function(method, file, date = NULL){
+  ## read the CSV data
+  data = read.csv(file)
+  #data = df
+  
+  ## Insert a row for July 2019
+  data[nrow(data) +1,] = c("July 1, 2019", NA)
+  
+  ## sort the df
+  data = data[order(as.Date(data$system.time_start, format = "%b %e, %Y")),]
+  rownames(data) = 1:nrow(data)
+  
+  ## cast to numeric dtype
+  data$mesic = as.numeric(data$mesic)
+  
+  ## function to interpolate with vals from same month in preceding and following years
+  replace_na_with_mean <- function(df) {
+    for (i in 1:ncol(df)) {
+      na_indices <- which(is.na(df[, i]))
+      for (j in na_indices) {
+        if (j == 1) {
+          df[j, i] <- df[j + 4, i]
+        } else if (j == nrow(df)) {
+          df[j, i] <- df[j - 4, i]
+        } else {
+          df[j, i] <- mean(c(df[j - 4, i], df[j + 4, i]))
+        }
+      }
+    }
+    return(df)
+  }
+  
+  ## apply the interpolation function to fill NAs
+  data_fill = replace_na_with_mean(data)
+  
+  if(method == "BCP"){
+    ## include a 'year' variable
+    data_fill$year = format(as.Date(data_fill$system.time_start, format = "%b %e, %Y"), "%Y")
+    
+    ## univariate
+    results_bcp = bcp(data_fill$mesic)
+    summary(results_bcp)
+    outplot = plot(results_bcp, main = "BCP output - Mesic probabilities of change",
+         xlab = "Date", xaxlab = data_fill$year ) ## FIGURE 3
+    
+  }
+  
+  if(method == "BFAST"){
+    ## create a time series object
+    data_ts = ts(data_fill$mesic, frequency = 4) ## update to be flexible to # of obs/cycle
+    
+    ## apply the BFAST function
+    fit = bfast(data_ts, h = 0.15, season = "harmonic")
+    
+    outplot = plot(fit, ANOVA = T, main = "BFAST output", xaxt = "n")
+  }
+  
+  if(method == "BEAST"){
+    start_date = as.Date(data_fill$date[1])
+    deltat = 4/12
+    #results_beast = beast(data_fill$mesic, start = start_Date, deltat = "3 months", dump.ci = T)
+    results_beast = beast(data_fill$mesic, start = as.Date('2004-6-1'), deltat = "3 months", dump.ci = T)
+    #outplot = plot(results_beast, interactive = T) 
+    outplot = plot(results_beast, interactive = F) ## MAKE DIMS BIGGER 
+  }
+  
+    
+  if(method == "BSTS"){
+    pred_count = length(ancFiles)
+    
+    for(i in pred_count){
+      ## make a variable with the values
+    } 
+    ## hard code pdsi for now
+    pdsi = read.csv(ancFiles)
+    
+    ## Include PDSI as a predictor
+    #data_bsts=  cbind(data_fill$mesic, pdsi_no2016$pdsi)
+    data_bsts_time =  zoo(cbind(data_fill$mesic, pdsi$pdsi), 
+                          as.Date(data_fill$system.time_start,format = "%b %e, %Y")) ## includes missing data period
+    
+    ## define pre and post restoration periods
+    pre_period = c(1,32)
+    post_period = c(33, 64)
+    
+    ## with date
+    pre_period_date = as.Date(c("Jun 1, 2004", "Sep 1,2011"), format = "%b %e, %Y")
+    post_period_date = as.Date(c("Jun 1, 2012", "Sep 1,2020"), format = "%b %e, %Y")
+    
+    #impact = CausalImpact(data_bsts, pre_period, post_period, model.args = list(nseasons = 4))
+    impact_time = CausalImpact(data_bsts_time, pre_period_date, post_period_date, 
+                               model.args = list(nseasons = 4))
+    
+    summary(impact) 
+    summary(impact_time) ## nearly identical results AND includes period of missing data...
+    
+    outplot = plot(impact_time) 
+    }
+  
+  return(outplot)
+}
+  
